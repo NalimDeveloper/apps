4817659a4723c88595170500f6d799a84333fecd
NalimDeveloper <nalimdeveloper@gmail.hu>
2015-02-20 14:29:52 +0100

    Eseménykezelés

---
+++ KisMilanMark_KIMWACG.KEFO/EsemenyKezeles/public_html/app.js
@@ -0,0 +1,17 @@
+function clickButton(e) {
+    console.log("start:clickButton");
+    e.target.innerHTML = "Kattintott";
+}
+            
+            
+            document.addEventListener("DOMContentLoaded", function(event) {
+                console.log("start:DOMContentLoaded");
+                var buttons=document.querySelectorAll("button");
+                
+                for (x=0;x<buttons.length;x++) {
+                    console.log(buttons[x]);
+                    buttons[x].addEventListener("click", clickButton); 
+                }
+
+                console.log("stop:DOMContentLoaded");
+            });
